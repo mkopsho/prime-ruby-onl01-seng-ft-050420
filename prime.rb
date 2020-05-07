f995c2fa2bb76cef2e02a39e19dacbdd2491ec56
Michael Kopsho <michael.kopsho@gmail.com>
2020-05-07 01:46:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(number)
+  if number == 2
+    return true
+  elsif number == 1 || number.even? || number.negative?
+    return false
+  else
+    range = (2...number).to_a
+    range.none? do |n|
+      number % n == 0
+    end
+  end
+end
